8402ff912f3e0cfd716a1390de14607be6a9be63
andothergames <32204909+andothergames@users.noreply.github.com>
2024-11-22 15:34:06 +0000

    refactor: convert isEven tests to deno

--- __tests__/03-is-even.test.js
+++ __tests__/03-is-even.test.js
@@ -1,21 +1,30 @@
-const { isEven } = require("../katas/03-is-even")
+import {
+  assertEquals,
+  assertStrictEquals,
+} from "https://deno.land/std/testing/asserts.ts";
 
-describe("isEven", () => {
-  test("returns a boolean", () => {
-    const input = 10;
-    const result = isEven(input);
-    expect(typeof result).toBe("boolean");
-  });
+import { isEven } from "../katas/03-is-even.js";
 
-  test("returns true when given integer is even", () => {
+Deno.test("isEven returns a boolean", () => {
+  const input = 10;
+  const result = isEven(input);
+  assertStrictEquals(typeof result, "boolean");
+});
+
+Deno.test(
+  "isEven returns true when given number is even",
+  () => {
     const input = 30;
     const result = isEven(input);
-    expect(result).toBe(true);
-  });
+    assertEquals(result, true);
+  }
+);
 
-  test("returns false when given integer is odd", () => {
+Deno.test(
+  "isEven returns false when given number is odd",
+  () => {
     const input = 43;
     const result = isEven(input);
-    expect(result).toBe(false);
-  });
-  });
+    assertEquals(result, false);
+  }
+);
