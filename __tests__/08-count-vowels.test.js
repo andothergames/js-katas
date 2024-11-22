871ac4de93892f9d49afb7bdef29b5beb0b41892
andothergames <32204909+andothergames@users.noreply.github.com>
2024-11-22 16:13:00 +0000

    test: add countVowels tests

---
+++ __tests__/08-count-vowels.test.js
@@ -0,0 +1,42 @@
+import {
+  assertEquals,
+  assertStrictEquals,
+} from "https://deno.land/std/testing/asserts.ts";
+
+import { countVowels } from "../katas/08-count-vowels.js";
+
+Deno.test("countVowels returns a number", () => {
+  const input = "hello";
+  const result = countVowels(input);
+  assertStrictEquals(typeof result, "number");
+});
+
+Deno.test("countVowels returns sum of counted vowels in given string", () => {
+  const input = "alienation";
+  const result = countVowels(input);
+  assertEquals(result, 6);
+});
+
+Deno.test("countVowels returns 0 when given string contains no vowels", () => {
+  const input = "hymn";
+  const result = countVowels(input);
+  assertEquals(result, 0);
+});
+
+Deno.test(
+  "countVowels returns sum of counted vowels in given string ignoring case sensitivity",
+  () => {
+    const input = "foUnDIng";
+    const result = countVowels(input);
+    assertEquals(result, 3);
+  }
+);
+
+Deno.test(
+  "countVowels returns sum of counted vowels in given string that contains spaces",
+  () => {
+    const input = "polar bears are ok";
+    const result = countVowels(input);
+    assertEquals(result, 7);
+  }
+);
