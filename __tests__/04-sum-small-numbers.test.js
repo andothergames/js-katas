c08f42fa6ce83c7ef934fd63b40ff09ba3ea2a14
andothergames <32204909+andothergames@users.noreply.github.com>
2024-11-22 15:38:56 +0000

    refactor: convert sumSmallNumbers tests to deno

--- __tests__/04-sum-small-numbers.test.js
+++ __tests__/04-sum-small-numbers.test.js
@@ -1,38 +1,41 @@
-const { sumSmallNumbers } = require("../katas/04-sum-small-numbers")
+import {
+  assertEquals,
+  assertStrictEquals,
+} from "https://deno.land/std/testing/asserts.ts";
 
-describe("sumSmallNumbers", () => {
-  test("returns an number", () => {
-    const input1 = 0;
-    const input2 = 0;
-    const result = sumSmallNumbers(input1, input2);
-    expect(typeof result).toBe("number");
-  });
+import { sumSmallNumbers } from "../katas/04-sum-small-numbers.js";
 
-  test("returns sum of given arguments when both numbers are under 10", () => {
-    const input1 = 2;
-    const input2 = 7;
-    const result = sumSmallNumbers(input1, input2);
-    expect(result).toBe(9);
-  });
+Deno.test("sumSmallNumbers returns a boolean", () => {
+  const input1 = 0;
+  const input2 = 0;
+  const result = sumSmallNumbers(input1, input2);
+  assertStrictEquals(typeof result, "number");
+});
 
-  test("returns one number below 10 when the other argument is above 10", () => {
-    const input1 = 8;
-    const input2 = 12;
-    const result = sumSmallNumbers(input1, input2);
-    expect(result).toBe(8);
-  });
+Deno.test("sumSmallNumbers returns sum of given arguments when both numbers are under 10", () => {
+  const input1 = 2;
+  const input2 = 7;
+  const result = sumSmallNumbers(input1, input2);
+  assertEquals(result, 9);
+});
 
-  test("returns one number below 10 when the other argument is above 10", () => {
-    const input1 = 90;
-    const input2 = 6;
-    const result = sumSmallNumbers(input1, input2);
-    expect(result).toBe(6);
-  });
+Deno.test("sumSmallNumbers returns one number below 10 when the other argument is above 10", () => {
+  const input1 = 8;
+  const input2 = 12;
+  const result = sumSmallNumbers(input1, input2);
+  assertEquals(result, 8);
+});
 
-  test("returns zero when both of the given arguments are 10 or above", () => {
-    const input1 = 10;
-    const input2 = 20;
-    const result = sumSmallNumbers(input1, input2);
-    expect(result).toBe(0);
-  });
-  });
+Deno.test("sumSmallNumbers returns one number below 10 when the other argument is above 10", () => {
+  const input1 = 90;
+  const input2 = 6;
+  const result = sumSmallNumbers(input1, input2);
+  assertEquals(result, 6);
+});
+
+Deno.test("sumSmallNumbers returns zero when both of the given arguments are 10 or above", () => {
+  const input1 = 10;
+  const input2 = 20;
+  const result = sumSmallNumbers(input1, input2);
+  assertEquals(result, 0);
+});
